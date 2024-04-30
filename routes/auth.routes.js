bf6872ef31f0590565e1d3d0e21c1a427c561c30
KemalGokten <ukemalgokten@gmail.com>
2024-04-30 16:43:52 +0200

    Refactor and format, Implement forgot password and reset password routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,24 @@
+//Module Imports
 const router = require("express").Router();
-const bcryptjs = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User.model");
+const nodemailer = require("nodemailer");
+
+//MidlleWare Imports
 const { isAuthenticated } = require("../middlewares/route-gaurd.middleware");
+
+//Models
+const User = require("../models/User.model");
+
+//Variables
+const saltCount = 13;
 
 // Sign up route
 router.post("/signup", async (req, res, next) => {
   const { password, ...userData } = req.body;
   try {
-    const salt = bcryptjs.genSaltSync(13);
-    userData.passwordHash = bcryptjs.hashSync(password, salt);
+    const salt = bcrypt.genSaltSync(saltCount);
+    userData.passwordHash = bcrypt.hashSync(password, salt);
     const newUser = await User.create(userData);
     const { passwordHash, ...user } = newUser._doc;
     res.status(201).json({ message: "success", data: user });
@@ -24,8 +33,11 @@ router.post("/login", async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const userData = await User.findOne({ email });
-    if (userData && bcryptjs.compareSync(password, userData.passwordHash)) {
-      const token = jwt.sign({ id: userData._id, isExpert }, process.env.SECRET_KEY, { algorithm: "HS256", expiresIn: "1h" });
+    if (userData && bcrypt.compareSync(password, userData.passwordHash)) {
+      const token = jwt.sign({ id: userData._id }, process.env.SECRET_KEY, {
+        algorithm: "HS256",
+        expiresIn: "1h",
+      });
       res.status(200).json({ message: "success", data: { token } });
     } else {
       res.status(401).json({ message: "Email or password is incorrect." });
@@ -36,9 +48,75 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
+//Forgot Password Route
+router.post("/forgot-password", async (req, res, next) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "5m",
+    });
+
+    // Send reset password email
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // Use `true` for port 465, `false` for all other ports
+      auth: {
+        user: process.env.APP_EMAIL,
+        pass: process.env.APP_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: "brainbash.jfk@gmail.com",
+      to: email,
+      subject: "Password Reset",
+      html: `<p>You can reset your password <a href="${process.env.CLIENT_URL}/reset-password/${token}">here</a></p>`,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+
+    res.status(200).json();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Reset password route
+router.post("/reset-password/:token", async (req, res, next) => {
+  const { token } = req.params;
+  const { newPassword } = req.body;
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    const user = await User.findById(decodedToken.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, saltCount);
+    user.passwordHash = hashedPassword;
+    await user.save();
+
+    res.status(200).json({ message: "Password reset successfully" });
+  } catch (err) {
+    next(err);
+  }
+});
+
 // Verify token route
 router.get("/verify", isAuthenticated, (req, res) => {
   res.status(200).json({ message: "success", data: req.payload });
-})
+});
 
 module.exports = router;
