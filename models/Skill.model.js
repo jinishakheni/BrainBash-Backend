341d65c30dd099e78e76e8fbd1635a5433e0f20a
KemalGokten <ukemalgokten@gmail.com>
2024-05-02 20:29:16 +0200

    Create Category and Skill models

--- models/Skill.model.js
+++ models/Skill.model.js
@@ -1,19 +1,23 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model, Types } = require("mongoose");
 
 const skillSchema = new Schema(
   {
-    skill: String,
-    categoryId: {
-      type: Tapes.ObjectId,
-      ref: "Category"
-    }
+    skillName: { type: String, required: true, unique: true },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.createdAt;
+        delete ret.updatedAt;
+        delete ret.__v;
+        return ret;
+      },
+    },
   }
-)
+);
 
-const Skill = model('Skill', skillSchema);
+const Skill = model("Skill", skillSchema);
 
 module.exports = Skill;
