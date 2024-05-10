d65b002c145bf58413877c77141f3634b6e25f83
KemalGokten <ukemalgokten@gmail.com>
2024-05-10 13:17:19 +0200

    Recieving senderEmail from the route

--- routes/contactUs.routes.js
+++ routes/contactUs.routes.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 const {sendEmailOnContactUs} = require("../services/emailService");
 
 router.post("/", async (req, res, next) => {
-  const { senderName, senderMessage } = req.body;
+  const { senderEmail,senderName, senderMessage } = req.body;
 
   try {
     if (senderName && senderMessage) {
-      await sendEmailOnContactUs(senderName, senderMessage);
+      await sendEmailOnContactUs(senderEmail, senderName, senderMessage);
       res.sendStatus(200);
     } else {
       res.status;
